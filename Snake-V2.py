9a51f9a046425fb816e98470a2aafdb63d9e9f5d
Marco <marcotecocha@gmail.com>
2021-07-08 10:33:20 -0500

    Version2

---
+++ Snake-V2.py
@@ -0,0 +1,58 @@
+import turtle
+import time
+
+posponer = 0.1
+
+#Configuración de Ventana
+wn = turtle.Screen()
+wn.title("Juego de Pong")
+wn.bgcolor("Black")
+wn.setup(width = 600, height = 600)
+wn.tracer(0)
+
+#Cabeza serpiente
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("white")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "stop"
+
+#Funciones
+def arriba():
+    cabeza.direction = "up"
+def abajo():
+    cabeza.direction = "down"
+def izquierda():
+    cabeza.direction = "left"
+def derecha():
+    cabeza.direction = "right"
+
+def mov():
+    if cabeza.direction == "up":
+        y = cabeza.ycor()
+        cabeza.sety(y + 20)
+    if cabeza.direction == "down":
+        y = cabeza.ycor()
+        cabeza.sety(y - 20)
+    if cabeza.direction == "left":
+        x = cabeza.xcor()
+        cabeza.setx(x - 20)
+    if cabeza.direction == "right":
+        x = cabeza.xcor()
+        cabeza.setx(x + 20)
+#Teclado
+wn.listen()
+wn.onkeypress(arriba, "Up")
+wn.onkeypress(abajo, "Down")
+wn.onkeypress(izquierda, "Left")
+wn.onkeypress(derecha, "Right")
+
+while True:
+    wn.update()
+
+    mov()
+    time.sleep(posponer)
+
+#Movimiento de la cabeza de la serpiente
